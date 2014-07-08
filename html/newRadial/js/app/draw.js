42bcb1a8f1622bfad277c0513f7ce99ee7711c4b
Rob Larock <rob.larock@gmail.com>
2014-07-07 21:23:58 -0700

    Fixed margins! (properly!)

--- html/newRadial/js/app/draw.js
+++ html/newRadial/js/app/draw.js
@@ -14,25 +14,28 @@ define(['canvas', 'app/wedge'], function(canvas, wedge)
 		        canvas.context.beginPath();
 
 		        // --- Wedge ---
-		        // should be defined by: 
+		        // Tunables
 		        var sHalfThetaRads = Math.PI/13;
 		        var vOrigin = { x : -.175 * canvas.canvas.width , y : canvas.canvas.height / 2 }
 		        var sArcRadius = -vOrigin.x + .6 * canvas.canvas.width;
-
+		        
 		        // "Consts"
 		        var bClockwise = false;
 		        var bIsLeftRadius = true;
 		        var bIsTopTheta = true;
 
-		        var leftRadius = wedge.sGetRingRadius(iRingIndex, bIsLeftRadius)
-		        var rightRadius = wedge.sGetRingRadius(iRingIndex, !bIsLeftRadius) 
-		        var topTheta = wedge.sGetRungTheta(iRingIndex, iRungIndex, bIsTopTheta);
-		        var bottomTheta = wedge.sGetRungTheta(iRingIndex, iRungIndex, !bIsTopTheta);
+		        var leftRadius = wedge.sGetRingRadius(iRingIndex, bIsLeftRadius);
+		        var rightRadius = wedge.sGetRingRadius(iRingIndex, !bIsLeftRadius);
+		        var topLeftTheta = wedge.sGetRungTheta(iRingIndex, iRungIndex, leftRadius, bIsTopTheta);
+		        var topRightTheta = wedge.sGetRungTheta(iRingIndex, iRungIndex, rightRadius, bIsTopTheta);
+		        var bottomLeftTheta = wedge.sGetRungTheta(iRingIndex, iRungIndex, leftRadius, !bIsTopTheta);
+		        var bottomRightTheta = wedge.sGetRungTheta(iRingIndex, iRungIndex, rightRadius, !bIsTopTheta);
+				
 				// left wedge
-		        canvas.context.arc(wedge.vOrigin.x, wedge.vOrigin.y, leftRadius, bottomTheta, topTheta, !bClockwise)
+		        canvas.context.arc(wedge.vOrigin.x, wedge.vOrigin.y, leftRadius, bottomLeftTheta, topLeftTheta, !bClockwise)
 
 		        // right wedge, implicitly connects top of two arcs
-		        canvas.context.arc(wedge.vOrigin.x, wedge.vOrigin.y, rightRadius, topTheta, bottomTheta, bClockwise);
+		        canvas.context.arc(wedge.vOrigin.x, wedge.vOrigin.y, rightRadius, topRightTheta, bottomRighetTheta, bClockwise);
 
 				// explicitly connects bottom of two arcs
 		        canvas.context.closePath();
@@ -46,6 +49,61 @@ define(['canvas', 'app/wedge'], function(canvas, wedge)
 		        canvas.context.lineWidth   = 5;
 		     //   canvas.context.stroke();
 		    }
-        }
+
+		    // TODO ROB
+		    /* when we draw the highlight, and are animating the highlight to another cell: consider moving from the top middle tile to the one below it
+		    - orange highlight around the top middle wedge
+		    - either: highlight breaks on the top straight segment, at its middle, and the orange lines run down each side to the slot below
+		    - or:each arc/rounded line segment slips down to the next tile. the bottom higlight stays there, becomes the new top edge higlight.
+				the final highlight either dissappears with the sides, or it runs from left to right out and slides left to right into the one below. could be rad.
+		    - */
+       }
     }
 );
+
+    function drawWedgeTileHighlight(iRingIndex, iRungIndex)
+    {
+        context.beginPath();
+
+        // todo rob obviously replace this horrible cut and paste code, but its pre-vis!
+
+        // --- Wedge ---
+        // should be defined by: 
+        var sHalfThetaRads = Math.PI/13;
+        var vOrigin = { x : -.175 * canvas.width , y : canvas.height / 2 }
+        var sArcRadius = -vOrigin.x + .6 * canvas.width;
+
+        // "Consts"
+        var bClockwise = false;
+        var bIsLeftRadius = true;
+        var bIsTopTheta = true;
+
+        var leftRadius = Wedge.sGetRingRadius(iRingIndex, bIsLeftRadius)
+        var rightRadius = Wedge.sGetRingRadius(iRingIndex, !bIsLeftRadius) 
+        var topTheta = Wedge.sGetRungTheta(iRingIndex, iRungIndex, bIsTopTheta);
+        var bottomTheta = Wedge.sGetRungTheta(iRingIndex, iRungIndex, !bIsTopTheta);
+        // left wedge
+        context.arc( Wedge.vOrigin.x, Wedge.vOrigin.y, leftRadius, bottomTheta, topTheta, !bClockwise)
+
+        // right wedge, implicitly connects top of two arcs
+        context.arc(Wedge.vOrigin.x, Wedge.vOrigin.y, rightRadius, topTheta, bottomTheta, bClockwise);
+
+        // explicitly connects bottom of two arcs
+        context.closePath();
+        
+        // fill
+        //context.fillStyle="white";
+        //context.fill();
+
+         // Set line stroke props
+        context.strokeStyle = "#FF6600";
+        context.lineWidth   = 5;
+        context.stroke();
+    }
+
+
+    function clearColour(colour)
+    {
+        context.fillStyle = colour;
+        context.fillRect(0,0,canvas.width, canvas.height);
+    }
--- html/newRadial/js/app/wedge.js
+++ html/newRadial/js/app/wedge.js
@@ -1,19 +1,20 @@
+// wedge.js
+// Essentially my layout calculator
 
 define(['math'], function(math) 
 {
+    // Tunables
     var _nRings = 3;
     var _nRungsPerRing = [1, 3, 4]; // panels per vert wedge slice, like [rob], [projectSource1, projSrc2, projSrc3], [pName1, pName2, ..]
-    var _vOrigin = { x : -.175 * canvas.width , y : canvas.height / 2 };
+    var _vOrigin = { x : -.2 * canvas.width , y : canvas.height / 2 };
     var _sSideMargin = .03;
-    var _sHalfThetaRads =  Math.PI/14;
-    var _sCellMargin = 0;
-    var _sCellMarginFixedHorz = 10;
-    var _sCellMarginFixedVert = .0086;
+    var _sHalfThetaRads =  Math.PI/13;
     var _sArcRadiusLeft = -_vOrigin.x + canvas.width * _sSideMargin;
     var _sArcRadiusRight = -_vOrigin.x + canvas.width * (1-_sSideMargin);
+    var _sCellMarginFixedHorz = .005 * canvas.width; // pixels
+    var _sCellMarginFixedVert = .01 * canvas.height; // radians of arg length.. yeah sorry
 
-    var vec = new math.Vector(1,2);
-
+     // todo: figure out how to calculate the usable text area in a wedge
     return {
         nRings: _nRings,
         nRungsPerRing: _nRungsPerRing, 
@@ -26,15 +27,28 @@ define(['math'], function(math)
         sGetRingRadius: function (iRingIndex, bIsLeftRadius)
         {
             iRingIndex += bIsLeftRadius ? 0 : 1;
-            var blend = (iRingIndex/_nRings) + (_sCellMargin * (bIsLeftRadius ? 1 : -1));
+            var blend = (iRingIndex/_nRings);
             return math.lerp(_sArcRadiusLeft, _sArcRadiusRight, blend) + _sCellMarginFixedHorz * (bIsLeftRadius ? 1 : -1);
+            // margin between wedge as pixels doesnt really work out, cause the blend is an angle.
+            // can we.. offset the origin?? if we take the same angle, 
         },
-        sGetRungTheta: function (iRingIndex, iRungIndex, bIsTopTheta)
+
+        // Get TODO ROB DOC
+        // radius is passed so we can do fixed margins. questionable if this should be here or in draw.js
+        sGetRungTheta: function (iRingIndex, iRungIndex, radius, bIsTopTheta)
         {
+            // so to accomplish fixed margins, we need to know the radius at which we want that arclength displacement.
+            // this depends on which rung were on, how many rungs there are, etc, so we make the caller give us the radius.
+            var fixedMargin = _sCellMarginFixedVert / radius; // fixed margins: theta = arcLength / radius
+
+            // the bottom of the wedge is 1/n -th of theta downwards
             iRungIndex += bIsTopTheta ? 0 : 1;
-            var blend = (iRungIndex / _nRungsPerRing[iRingIndex]) + (_sCellMargin * (bIsTopTheta ? 1 : -1));
-            return math.lerp(-_sHalfThetaRads, _sHalfThetaRads, blend) + _sCellMarginFixedVert * (bIsTopTheta ? 1 : -1);
+            var blend = (iRungIndex / _nRungsPerRing[iRingIndex]);
+
+            return math.lerp(-_sHalfThetaRads, _sHalfThetaRads, blend) + fixedMargin * (bIsTopTheta ? 1 : -1);
         },
+
+        // Get a point relative to the origin of the wedge
         vGetPoint: function(theta, radius)
         {
             var x = (_vOrigin.x + Math.cos(theta) * radius);
@@ -43,68 +57,7 @@ define(['math'], function(math)
             return math.Vector(x, y);;
         }
     }
-/*
-    // Tunables - params for the size and shape of the wedge
-    var _nRings = 3;
-    var _nRungsPerRing = [1, 3, 4]; // panels per vert wedge slice, like [rob], [projectSource1, projSrc2, projSrc3], [pName1, pName2, ..]
-    var _vOrigin = { x : -.175 * canvas.width , y : canvas.height / 2 };
-    var _sSideMargin = .03;
-    var _sHalfThetaRads =  Math.PI/14;
-    var _sCellMargin = 0;
-    var _sCellMarginFixedHorz = 10;
-    var _sCellMarginFixedVert = .0086;
-    return
-    {
-        nRings: _nRings,
-        nRungsPerRing: _nRungsPerRing, 
-
-        vOrigin: _vOrigin,
-        sSideMargin: _sSideMargin,
-        sHalfThetaRads: _sHalfThetaRads,
-        sArcRadiusLeft: -_vOrigin.x + canvas.width * _sSideMargin, 
-        sArcRadiusRight: -_vOrigin.x + canvas.width * (1-_sSideMargin),
-        sGetRingRadius: function (iRingIndex, bIsLeftRadius)
-        {
-            iRingIndex += bIsLeftRadius ? 0 : 1;
-            var blend = (iRingIndex/nRings) + (_sCellMargin * (bIsLeftRadius ? 1 : -1));
-            return lerp(Wedge.sArcRadiusLeft, Wedge.sArcRadiusRight, blend) + _sCellMarginFixedHorz * (bIsLeftRadius ? 1 : -1);
-        },
-        sGetRungTheta: function (iRingIndex, iRungIndex, bIsTopTheta)
-        {
-            iRungIndex += bIsTopTheta ? 0 : 1;
-            var blend = (iRungIndex / nRungsPerRing[iRingIndex]) + (_sCellMargin * (bIsTopTheta ? 1 : -1));
-            return lerp(-Wedge.sHalfThetaRads, Wedge.sHalfThetaRads, blend) + _sCellMarginFixedVert * (bIsTopTheta ? 1 : -1);
-        },
-        vGetPoint: function(theta, radius)
-        {
-            // TODO ROB i must get a vector class in this project before effing with particle systems. this function i even dont like
-            return 
-            {
-                x : Wedge.vOrigin.x + Math.cos(theta) * radius, 
-                y : Wedge.vOrigin.y + Math.sin(theta) * radius
-            };
-        }
-    }*/
 });
 
-/*
-    // Wedge division props
-    // picture this like a spreadsheet, with vertically merged cells, in polar coordinates.
-    var nRings = 3;
-    var nRungsPerRing = [1, 3, 4]; // [rob], [projectSource1, projSrc2, projSrc3], [pName1, pName2, ..]
-    
-    function lerp(x, y, t)
-    {
-        return x + t * (y-x);
-    }
-
-    // Wedge tunables
-    // todo: figure out how to calculate the usable text area in a wedge
-	var Wedge;
-    function initWedge()
-    {
-        
-
-        Wedge = 
-        
-    }*/
+// TODO someday
+/* some kinda reflow work which will move the tile layout and resize automatically based on the resolution.*/
